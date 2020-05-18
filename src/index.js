dde5453a59a3bc120400384b12b9d35bae67ae44
Suntry0514 <iniesta_0413@yahoo.co.jp>
2020-05-18 17:10:06 +0900

    fix tyeping miss

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import reducer from './reducers'
 import App from './components/App'
 
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(reducer)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 /*全リデゥーサーを１つに結合する*/
 import {combineReducers} from 'redux'//リデゥーサーを結合するための関数
-import cout from './count'
+import count from './count'
 
 export default combineReducers({ count})
 //以下のように複数記述することで１つにまとめられる
