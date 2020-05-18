c613f0bbdecaf516aeb9e588f51f1602a0a16503
Suntry0514 <iniesta_0413@yahoo.co.jp>
2020-05-18 16:23:56 +0900

    implement action and action creater

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+//Action Creater
+export const increment = () => ({
+        type: 'INCREMENT'
+    })
+
+export const decrement = () => ({
+        type: 'DECREMENT'
+    })
+
+/* export const increment = () => {
+    return{
+        type: 'DECREMENT'
+    }
+}
+ */
