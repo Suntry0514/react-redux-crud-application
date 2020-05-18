811181cec89270d885c6cf2a2e214406cd0ed097
Suntry0514 <iniesta_0413@yahoo.co.jp>
2020-05-18 13:53:08 +0900

    コンポーネントを2種類定義

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from '@testing-library/react';
 
-function App() {
+//functionによるコンポーネント
+/* function App() {
   const greeting = "Hi, Jack!";
   const dom = <h1 className="foo">{greeting}</h1>
   return (
     <React.Fragment>
       {dom}
       <label htmlFor="bar">bar</label>
-      <input id="bar" type="text" onChange={()=>{console.log("I am clicked!")}}/>
+      <input id="bar" type="text" onChange={() => { console.log("I am clicked!") }} />
 
     </React.Fragment>)
   //return <div><h1>Hellow World!</h1></div>;
+} */
 
-
+//クラスによるコンポーネント
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>コンポーネント</h1>
+      </React.Fragment>
+    )
+  }
 }
 
+//実行するコンポーネントを指定
 export default App;
